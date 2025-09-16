219822a574cc0563e1e710abe32c4755aca79e6b
tnaldinho <dev@tnaldinho.com>
2025-09-16 13:22:19 +0200

    Add opcode logic

--- rtl/alu.sv
+++ rtl/alu.sv
@@ -9,6 +9,19 @@ module alu #(
   );
 
   // Complete your RTL code here
+  always_comb begin
+    case (opcode)
+      4'b0000: out = in_a + in_b; // ADD
+      4'b0001: out = in_a - in_b; // SUB
+      4'b0010: out = in_a & in_b; // AND
+      4'b0011: out = in_a | in_b; // OR
+      4'b0100: out = in_a ^ in_b; // XOR
+      4'b0101: out = in_a + 1;       // Increment
+      4'b0110: out = in_a;   // Passthrough A
+      4'b0111: out = in_b;   // Passthrough B
+      default: out = '0;          // NOP or undefined opcode
+    endcase
+  end
 
 endmodule
 
